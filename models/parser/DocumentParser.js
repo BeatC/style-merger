d925e75319644043b64f2e88764e54498e039535
Ilya Daynatovich <IlyaD@arkadium.com>
2016-02-20 21:57:49 +0200

    Added test file and DocumentParser and Document object model

--- models/parser/DocumentParser.js
+++ models/parser/DocumentParser.js
@@ -1,11 +1,37 @@
 'use strict';
 
+var ElementParser = require('./ElementParser.js');
+var Document = require('../object-model/Document.js');
+
 var DocumentParser = function (options) {
     options = options || {};
     
-    this._elements = options.elements || [];
+    this.documentPattern = /([#\.\-\w\s:]+\{[\s\w:\-%!\(\)\.\,#;"']*\})/gi;
 }
 
-
+DocumentParser.prototype.parseDocument = function (documentStr) {
+    var element;
+    var elementsRawList = [];
+    
+    while (true) {
+        element = this.documentPattern.exec(documentStr);
+        
+        if (element === null) {
+            break;
+        }
+        
+        element = element[0];
+        elementsRawList.push(element.trim()); 
+    }
+    
+    var elementsList = elementsRawList.map(function (element) {
+        var parser = new ElementParser();
+        return parser.parseElement(element);
+    }).filter(function (el) { return el !== null });
+    
+    return new Document({
+        elements: elementsList
+    });
+};
 
 module.exports = DocumentParser;
--- models/parser/ElementParser.js
+++ models/parser/ElementParser.js
@@ -1,47 +1,54 @@
 'use strict';
 
-var extend = require('../util/extend.js');
-var Rule = require('../Rule.js');
-var Element = require('../Element.js');
-var Selector = require('../Selector.js');
+var Rule = require('../object-model/Rule.js');
+var Element = require('../object-model/Element.js');
+var Selector = require('../object-model/Selector.js');
 
-var CssParser = function (options) {
+var ElementParser = function (options) {
     options = options || {};
     
-    this._elementPattern = /(.*)\{\s(\s*.*\s)*\}/gi;
+    this._elementPattern = /(.*)\{([\w\-:;!%\(\)\,\.#\s]+)\}/gi;
 };
 
-CssParser.prototype.parseElement = function (elementString) {
+ElementParser.prototype.parseElement = function (elementString) {
     var result = this._elementPattern.exec(elementString);
     
-    // Divide by elements
-    var selectors = result[1].split(/\s/).filter(function (el) { return el !== ''; });
-    
-    var selector = new Selector({
-        selectors: selectors
-    })
-    
-    var selectorList = [];
-    selectorList.push(selector);
-    
-    // Divide by rules
-    var rules = result[2].trim()
-    .split(/;/)
-    .filter(function (el) { 
-        return el !== ''; 
-    }).map(function (rule) {
-        var pair = rule.split(/[\:\s]+/)
+    console.log(result);
+    if (result !== null) {
+        // Divide by elements
+        var selectors = result[1].split(/\s/).filter(function (el) { return el !== ''; });
+        
+        var selector = new Selector({
+            selectors: selectors
+        })
+        
+        var selectorList = [];
+        selectorList.push(selector);
+        
+        // Divide by rules
+        var rules = result[2].trim()
+        .split(/;/)
         .filter(function (el) { 
-            return el !== '';
+            return el !== ''; 
+        }).map(function (rule) {
+            var pair = rule.split(/[:]+/gi)
+            .map(function (el) {
+                return el.trim();
+            })
+            .filter(function (el) { 
+                return el !== '';
+            });
+            
+            return new Rule(pair[0], pair[1]);
         });
         
-        return new Rule(pair[0], pair[1]);
-    });
+        return new Element({
+            selectors: selectorList,
+            rules: rules
+        })   
+    }
     
-    return new Element({
-        selectors: selectorList,
-        rules: rules
-    })
+    return null;
 };
 
-module.exports = CssParser;
+module.exports = ElementParser;
