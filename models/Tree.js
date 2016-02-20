e4ef05fa5b5cfa08af16da2031dcea16ab1bf9c5
Михаил Чирков < >
2016-02-20 21:58:37 +0200

    refactoring Node class & Tree one

---
+++ models/Tree.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Node = require("./Node.js");
+var Document = require("../parser/DocumentParser.js")
+
+function Tree (root) {
+	this.root = root;
+}
+Tree.prototype.merge = function() {
+	
+};
+Tree.prototype.add = function() {
+	// body...
+};
+Tree.prototype.remove = function() {
+	// body...
+};
+Tree.prototype.getDocument = function() {
+	// body...
+};
